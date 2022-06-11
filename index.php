952fe821cfb10187d5950fbd8a23f3d9797707d7
melihyldz10 <77010452+melihyldz10@users.noreply.github.com>
2022-06-11 17:08:22 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> declare a variable inside a variable</title>
+</head>
+<body>
+<?
+
+$example    = "Example";
+$example2   = "Example2";
+$example3   = "$example and $example2 are combined.";
+
+echo  $example3;
+
+?>
+</body>
+</html>
